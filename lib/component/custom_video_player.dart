873f4f3b9d5b803e5b5b75bd3378dc9ddbb31a12
meteor <tjdans34568@gmail.com>
2023-02-03 08:55:14 +0900

    20230203 Meteor : [CODE] 공통 컴포넌트 분리 작업 시작

---
+++ lib/component/custom_video_player.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class CustomVideoPlayer extends StatefulWidget {
+  const CustomVideoPlayer({Key? key}) : super(key: key);
+
+  @override
+  State<CustomVideoPlayer> createState() => _CustomVideoPlayerState();
+}
+
+class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
